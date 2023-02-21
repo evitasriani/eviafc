dd61d6778935c2792eb3beea16d3b28e6abd35b9
Andrew Patrick de Fretes <andrewbelanda@gmail.com>
2023-02-21 13:13:26 +0900

    add

---
+++ function.js
@@ -0,0 +1,32 @@
+//smoothscroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 2000); /* KECEPATAN SCROLL, DEFAULT 1000 dari kita*/
+        return false;
+      }
+    }
+  });
+});
+
+
+$(document).ready(function() {
+  $(".logo").on("click", function() {
+    $("header nav ul").toggleClass("open");
+  });
+});
+
+
+jQuery(document).ready(function(){
+  jQuery('.skillbar').each(function(){
+    jQuery(this).find('.skillbar-bar').animate({
+      width:jQuery(this).attr('data-percent')
+    },6000);
+  });
+});
+
